5c6872179cdbab16197865a3308e5e1f4fbae15b
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-09-20 15:05:56 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -2,3 +2,4 @@ const add = (a,b) => {return a+b }
 const subtract = (a,b) => {return a-b} 
 const multiply = (a,b) => {return a*b} 
 const devide = (a,b) => {return a/b} 
+const mod = (a,b) => {return a%b} 
