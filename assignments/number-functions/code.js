4d4570cd508aa01caf4d9742164db8b101c6c2bd
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-09-20 15:25:33 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -6,3 +6,4 @@ const mod = (a,b) => {return a%b}
 const averageOfTwo = (a,b) => {return (a+b)/2} 
 const averageOfThree = (a,b,c) => {return (a+b+c)/3}   
 const distance = (a,b) => {return Math.abs (a-b)} 
+const manhattanDistance = (a,b,c,d) => {return Math.abs(a-c)+Math.abs (b-d)}
