3cb78dcc6bdc4958ce26595c967492e8f650f49d
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-09-21 14:58:32 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,7 +1,9 @@
 const add = (a,b) => {return a+b }
 const subtract = (a,b) => {return a-b} 
 const multiply = (a,b) => {return a*b} 
-const devide = (a,b) => {return a/b} 
+const divide = (a, b) => {
+  return a/b
+} 
 const mod = (a,b) => {return a%b} 
 const averageOfTwo = (a,b) => {return (a+b)/2} 
 const averageOfThree = (a,b,c) => {return (a+b+c)/3}   
