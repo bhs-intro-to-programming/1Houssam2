7f784be6c9073ea10a3f6d29245b5b3076ea86cc
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-09-27 14:48:04 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -26,4 +26,5 @@ const eatsFish= ( _isRed, _isSpotted) => {return !_isRed || _isSpotted}
 const eatsMice = ( _isRed, _isSpotted) => {return !_isRed || !_isSpotted} 
 const isRed = (birdR) => { return birdR === "Flobby" || birdR === "Bloggy"} 
 const isSpotted = (birdS) => {return birdS === "Flobby" || birdS === "Flibble"} 
-const isNotRed = (birdNR) => {return birdNR === "Flibble" || birdNR === "Globby"} 
+const isNotRed = (birdNR) => {return birdNR === "Flibble" || birdNR === "Globby"}
+const isNotSpotted = (birdNS) => {return birdNS === "Globby" || birdNS === "Bloggy"} 
