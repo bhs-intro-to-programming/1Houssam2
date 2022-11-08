fccd2c7dd0ce2515babbcf9fe551a8d5e64e28d3
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-11-08 15:02:08 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -7,6 +7,6 @@ const point = (x,y) => {
 const emptyObject = () => {
   return {} 
 }
-const distance = (x,y) => {
-  return {x,y}
+const distance = (p1,p2) => {
+  return Math.sqrt( (p1.x - p2.x) **2 + (p1.y - p2.y) **2   )
 }
