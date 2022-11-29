8f302438e6c88a7ee8e9f160c6b8bc47aa937f02
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-11-29 14:54:54 -0800

    Updating assessments/2022-11-29-numeric/code.js

---
+++ assessments/2022-11-29-numeric/code.js
@@ -0,0 +1,13 @@
+const totalEggs = (hardBoiledEggs, softBoiledEggs) => { 
+  return hardBoiledEggs+softBoiledEggs
+} 
+const chocolatesPerPerson = (chocolates, people) => { 
+  return chocolate/people
+} 
+const extraChocolates = ( chocolates, people) => { 
+  return chocolates/people
+} 
+const leftOut = (chocolate, people) => { 
+  return chocolate%people
+} 
+const probability 
