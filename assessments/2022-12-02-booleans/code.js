ead4d1b23be5ef3ff128e74afc8cbcf960f34796
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-12-02 14:51:02 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,1 +1,7 @@
-const needHeavyCoat = 
+const needHeavyCoat = (goingoutside, somewherecold) => {
+  return goingoutside && somewherecold
+}
+const needSunScreen = (goingtothebeach, goingskiing) => { 
+  return goingtothebeach &&  goingskiing 
+} 
+const 
