921a2afb0ecdba1d12a1420db995734cfd2d03ff
1Houssam2 <111812492+1Houssam2@users.noreply.github.com>
2022-12-16 10:56:15 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,24 +1,3 @@
 const needHeavyCoat = (goingoutside, somewherecold) => {
   return goingoutside && somewherecold
 }
-const needSunScreen = (goingtothebeach, goingskiing) => { 
-  return goingtothebeach &&  goingskiing 
-} 
-const needMittens = (goingoutside, somewherewarm) => {
-  return goingoutside && ! somewherewarm 
-} 
-const isVenomous = ( stripedsnake, bluehead) => {
-  return stripedsnake && ! bluehead 
-} 
-const okaySpeed = (speedlimit10MPH, carspeedMPH) => {
-  return speedlimit10MPH && carspeedMPH ; 10
-}
-const twiceAsExpensive = (firstitem, seconditem) => {
-  return firstitem *2> seconditem 
-}
-const winningRecord = (numberofwinning, numberoflosses, numberofties) => { 
-  return  numberofwinning > numberoflosses && numberofties
-} 
-const isMagicNumber = ( number42, number17) => { 
-  return number42 || number17 
-}
